1f3a4bf1644f01e714064b9430cddf2577eb8947
ACE User <aceuser@e140-s20.lab.lagcc>
2017-06-05 21:21:55 -0400

    completed exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("This works!");
+// This declares the variable myName and assigns it the string value of my name
+var myName = 'Hernan Murcia';
+// This declares var myAge variable and sets it to a number
+var myAge = 27;
+// This declares  var myHobbies and assings it to an array or strings
+var myHobbies = ['Soccer','Ping Pong','Movies'];
+// This declares var myDog as an object and assigns it varios properties
+var myDog = {
+  name: 'Apollo',
+  age: 7,
+  likes: ['Playing ball','walks','eating'];
+};
+
+/*
+console.log(myName);
+console.log(myAge);
+console.log(myHobbies);
+*/
+// Object properties are accessed via dot notation
+console.log(myDog.name);
+console.log(myDog.age);
+// Items within an array can be accessed via [], arrays start counting from 
+console.log(myDog.likes[0]);
